19255c7124cc9039d846479de588d5f9e8b951cd
Vadim Gerasimov <gerasmeb34@gmail.com>
2022-06-07 05:55:46 +0300

    add pkg

--- main/main.go
+++ main/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	branch3 "CoursesIntelias/go_cources/branch_3"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello :)")
 	fmt.Println("Go!!!")
 	fmt.Println("GOOOOO")
+	branch3.Branch3()
 }
