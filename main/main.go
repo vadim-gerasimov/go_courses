b69623a2bcd9174c94b01008b3ff60bffe6da111
Vadim Gerasimov <gerasmeb34@gmail.com>
2022-06-16 17:58:39 +0300

    homework fix

--- main/main.go
+++ main/main.go
@@ -4,24 +4,26 @@ import "fmt"
 
 const (
 	applePrice float64 = 5.99
-	pearPrice  int     = 7
-	ourMoney   int     = 23
+	pearPrice  float64 = 7
+	ourMoney   float64 = 23
 )
 
 func main() {
 	applesToBuy := 9
 	pearsToBuy := 8
-	price := (applePrice * float64(applesToBuy)) + float64(pearPrice*pearsToBuy)
+	price := (applePrice * float64(applesToBuy)) + (pearPrice * float64(pearsToBuy))
 	fmt.Printf("1. We need %.2f UAH to buy %v apples and %v pears.\n", price, applesToBuy, pearsToBuy)
 
 	weCanBuyPears := ourMoney / pearPrice
-	fmt.Printf("2. We can buy %v pears.\n", weCanBuyPears)
+	fmt.Printf("2. We can buy %v pears.\n", int(weCanBuyPears))
 
 	weCanBuyApples := float64(ourMoney) / applePrice
 	fmt.Printf("3. We can buy %v apples.\n", int(weCanBuyApples))
 
 	wantApples := 2
 	wantPears := 2
-	canBuy := float64(pearPrice*wantPears)+(applePrice*float64(wantApples)) <= float64(ourMoney)
+	priceForApples := applePrice * float64(wantApples)
+	priceForPears := pearPrice * float64(wantPears)
+	canBuy := priceForApples+priceForPears <= float64(ourMoney)
 	fmt.Printf("4. Can we buy %v pears and %v apples? %t \n", wantApples, wantPears, canBuy)
 }
