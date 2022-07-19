8a852236e6708e9e2b641390cda559159213cd1a
Vadim Gerasimov <gerasmeb34@gmail.com>
2022-07-20 00:39:10 +0300

    cuncurrency

---
+++ homeWorks/concurrency/task1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	n := [][]int{
+		{2, 6, 9, 24},
+		{7, 3, 94, 3, 0},
+		{4, 2, 8, 35},
+		{8, -9},
+		{},
+		{0},
+		{-1, -20},
+	}
+	var wg sync.WaitGroup
+	for i, v := range n {
+		wg.Add(1)
+		go func(v []int, i int) {
+			defer wg.Done()
+			sum(v, i)
+		}(v, i)
+	}
+	wg.Wait()
+}
+
+func sum(v []int, i int) {
+	sum := 0
+	for _, val := range v {
+		sum += val
+	}
+	fmt.Printf("slice %d: %d\n", i, sum)
+}
